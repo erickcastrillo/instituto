3456f8039f6112c611b59325d6405da2c176da29
Erick Castrillo <erick.castrillo@gmail.com>
2022-11-24 21:13:40 -0600

    Creadas las clases de Persona y Usuario

---
+++ src/instituto/data/Usuario.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+package instituto.data;
+
+/**
+ *
+ * @author Erick Castrillo Arroyo <ecastrillo@edu.upolitecnica.cr>
+ * 603630082EC
+ */
+public class Usuario {
+
+}
